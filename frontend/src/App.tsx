9264c2240a07655434942e2e4a11b040ba24699a
fyzanshaik <fyzan.shaik@gmail.com>
2024-10-15 16:06:07 +0530

    Backend files were added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from './components/ui/button';
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+	return (
+		<div className="flex items-center justify-center h-screen bg-zinc-700">
+			<Button className="  m-5">Add Button</Button>
+		</div>
+	);
 }
