1a5098d0e42c99021617117610bbb743c15d2419
fyzanshaik <fyzan.shaik@gmail.com>
2024-11-10 21:34:57 +0530

    changing

--- backend/files/3/1/ECE/MC/mc6.c
+++ backend/files/3/1/ECE/MC/mc6.c
@@ -1,50 +1,49 @@
-//6. SEVEN SEGMENT DISPLAY 
+// 6. SEVEN SEGMENT DISPLAY
 
-#include <reg51.h> 
-#define DATA P2 
-void delay(unsigned int );        
- 
-int main() //main starting 
-{ 
- while(1) 
- { 
-  DATA=0xc0; 
-  delay(500); 
-   
-  DATA=0xf9; 
-  delay(500); 
-   
-  DATA=0xa4; 
-  delay(500); 
-   
-  DATA=0xb0; 
-  delay(500); 
-   
-  DATA=0x99; 
-  delay(500); 
-   
-  DATA=0x92; 
-  delay(500); 
-   
-  DATA=0x82; 
-  delay(500); 
-   
-  DATA=0xf8; 
-  delay(500); 
-   
-  DATA=0x80; 
-  delay(500); 
-   
-  DATA=0x90; 
-  delay(500); 
- } 
- 
-} 
- 
-void delay(unsigned int t) //delay    t*25msec 
-{ 
-unsigned int i,j; 
-for(i=0; i<t; i++) 
-for(j=0; j<1275; j++); 
-} 
- 
+#include <reg51.h>
+#define DATA P2
+void delay(unsigned int);
+
+int main() // main starting
+{
+    while (1)
+    {
+        DATA = 0xc0;
+        delay(500);
+
+        DATA = 0xf9;
+        delay(500);
+
+        DATA = 0xa4;
+        delay(500);
+
+        DATA = 0xb0;
+        delay(500);
+
+        DATA = 0x99;
+        delay(500);
+
+        DATA = 0x92;
+        delay(500);
+
+        DATA = 0x82;
+        delay(500);
+
+        DATA = 0xf8;
+        delay(500);
+
+        DATA = 0x80;
+        delay(500);
+
+        DATA = 0x90;
+        delay(500);
+    }
+}
+
+void delay(unsigned int t) // delay    t*25msec
+{
+    unsigned int i, j;
+    for (i = 0; i < t; i++)
+        for (j = 0; j < 1275; j++)
+            ;
+}
--- backend/files/3/1/ECE/MC/mc8.c
+++ backend/files/3/1/ECE/MC/mc8.c
@@ -1,47 +1,47 @@
-//stepper motor
-//CLOCKWISE
+// stepper motor
+// CLOCKWISE
 
-#include <REG51XD2.H> 
-void delay(unsigned int x) 
-{ 
-for(;x>0;x--); 
-} 
-void main() 
-{ 
-unsigned char val,i; 
-P0=0x88; 
-while (1) 
-{ 
-val=0x88; 
-for (i=0;i<4;i++) 
-{ 
-P0=val; 
-val=val>>1; 
-delay(575); 
-} 
-} 
-} 
+#include <REG51XD2.H>
+void delay(unsigned int x)
+{
+    for (; x > 0; x--);
+}
+void main()
+{
+    unsigned char val, i;
+    P0 = 0x88;
+    while (1)
+    {
+        val = 0x88;
+        for (i = 0; i < 4; i++)
+        {
+            P0 = val;
+            val = val >> 1;
+            delay(575);
+        }
+    }
+}
 
-//COUNTER CLOCKWISE 
+// COUNTER CLOCKWISE
 
-#include <REG51XD2.H> 
-void delay(unsigned int x) 
-{ 
-for(;x>0;x--); 
-} 
-void main() 
-{ 
-unsigned char val,i; 
-P0=0x11; 
-8. STEPPER MOTOR 
-while (1) 
-{ 
-val=0x11; 
-for (i=0;i<4;i++) 
-{ 
-P0=val; 
-val=val<<1; 
-delay(575); 
-} 
-} 
+#include <REG51XD2.H>
+void delay(unsigned int x)
+{
+    for (; x > 0; x--)
+        ;
+}
+void main()
+{
+    unsigned char val, i;
+    P0 = 0x11;
+    8. STEPPER MOTOR while (1)
+    {
+        val = 0x11;
+        for (i = 0; i < 4; i++)
+        {
+            P0 = val;
+            val = val << 1;
+            delay(575);
+        }
+    }
 }
